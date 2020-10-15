c815f2ce3978125060325b1e6f9ddfdf518c71eb
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:12:50 +0800

    [FizzBuzz] [Joseph] Test: Applied the test for when_the_given_is_multiple_of_3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
     @Test
     void should_return_order_number_when_the_given_is_not_multiple_of_3_5_or_7() {
         int orderNumber = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.countOff(orderNumber);
         assertEquals("1", actual);
+    }
+
+    @Test
+    void should_return_fizz_when_the_given_is_multiple_of_3() {
+        int orderNumber = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actual = fizzBuzz.countOff(orderNumber);
+        assertEquals("Fizz", actual);
     }
 }
