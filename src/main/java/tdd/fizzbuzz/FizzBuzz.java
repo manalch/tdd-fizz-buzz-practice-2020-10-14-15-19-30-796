9342b23c9ac8c8d7d0290f698aeea0c7068f4b19
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:31:59 +0800

    [FizzBuzz] [Joseph] Refactor: Refactored to make the modulo and return value a constant and extracted the condition into a method to provide better business value

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,18 +4,24 @@ public class FizzBuzz {
 
     private static final int MODULO_3 = 3;
     private static final String FIZZ = "Fizz";
+    private static final int MODULO_5 = 5;
+    private static final String BUZZ = "Buzz";
 
     public String countOff(int orderNumber) {
         if (isMultipleOfThree(orderNumber)) {
             return FIZZ;
         }
-        if (orderNumber % 5 == 0){
-            return "Buzz";
+        if (isMultipleOfFive(orderNumber)){
+            return BUZZ;
         }
         return String.valueOf(orderNumber);
     }
 
     private boolean isMultipleOfThree(int orderNumber) {
         return orderNumber % MODULO_3 == 0;
+    }
+
+    private boolean isMultipleOfFive(int orderNumber) {
+        return orderNumber % MODULO_5 == 0;
     }
 }
