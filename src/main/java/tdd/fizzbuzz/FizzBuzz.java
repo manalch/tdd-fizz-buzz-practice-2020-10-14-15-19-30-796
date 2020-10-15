a819d8734f4971ff9f4ea48c7651b01e71a15b60
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:19:18 +0800

    [FizzBuzz] [Joseph] Refactor: Refactored the fizz value into a constant and extracted the condtion of MODULO_3 since it has a duplicate.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,15 +5,20 @@ public class FizzBuzz {
     private static final int MODULO_3 = 3;
     private static final int MODULO_5 = 5;
     private static final int MODULO_7 = 7;
+    private static final String FIZZ = "Fizz";
 
     public String countOff(int orderNumber) {
-        if (orderNumber % MODULO_3 == 0) {
-            return "Fizz";
+        if (isMultipleOfThree(orderNumber)) {
+            return FIZZ;
         }
-        return orderNumber % MODULO_3 == 0
+        return isMultipleOfThree(orderNumber)
                 || orderNumber % MODULO_5 == 0
                 || orderNumber % MODULO_7 == 0
                 ? null
                 : String.valueOf(orderNumber);
+    }
+
+    private boolean isMultipleOfThree(int orderNumber) {
+        return orderNumber % MODULO_3 == 0;
     }
 }
