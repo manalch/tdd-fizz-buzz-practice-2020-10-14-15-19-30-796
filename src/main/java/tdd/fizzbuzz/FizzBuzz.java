dc23aa3b435ac972674ef81dadaf3c1d181b4fb5
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:27:34 +0800

    [FizzBuzz] [Joseph] Refactor : refactored the handling of a normal orderNumber and removed the constants that is not needed yet

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,19 +3,13 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     private static final int MODULO_3 = 3;
-    private static final int MODULO_5 = 5;
-    private static final int MODULO_7 = 7;
     private static final String FIZZ = "Fizz";
 
     public String countOff(int orderNumber) {
         if (isMultipleOfThree(orderNumber)) {
             return FIZZ;
         }
-        return isMultipleOfThree(orderNumber)
-                || orderNumber % MODULO_5 == 0
-                || orderNumber % MODULO_7 == 0
-                ? null
-                : String.valueOf(orderNumber);
+        return String.valueOf(orderNumber);
     }
 
     private boolean isMultipleOfThree(int orderNumber) {
