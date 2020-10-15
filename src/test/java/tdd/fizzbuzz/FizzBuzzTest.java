06983a3de8face497d46fe3a2ef627759fec390c
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 13:58:24 +0800

    [FizzBuzz] [Joseph] Test: Applied test case for order_number_when_the_given_is_not_multiple_of_3_5_or_7

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_number_when_the_given_is_not_multiple_of_3_5_or_7() {
+        int orderNumber = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actual = fizzBuzz.countOff(orderNumber);
+        assertEquals("1", actual);
+    }
 }
