5e80b63324f9629caaffab5c7574b403e176dcc3
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:03:55 +0800

    [FizzBuzz] [Joseph] Feature: Applied the development to handle when_the_given_is_not_multiple_of_3_5_or_7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOff(int orderNumber) {
+        return orderNumber %3 == 0 || orderNumber %5 == 0 || orderNumber %7 == 0 ? null : String.valueOf(orderNumber);
+    }
 }
