1a6ed29569983e501507ed9f7186a24e01faaf1c
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 14:08:38 +0800

    [FizzBuzz] [Joseph] Refactor: Refactored special numbers to constants

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,16 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    private static final int MODULO_3 = 3;
+    private static final int MODULO_5 = 5;
+    private static final int MODULO_7 = 7;
+
     public String countOff(int orderNumber) {
-        return orderNumber %3 == 0 || orderNumber %5 == 0 || orderNumber %7 == 0 ? null : String.valueOf(orderNumber);
+        return orderNumber % MODULO_3 == 0
+                || orderNumber % MODULO_5 == 0
+                || orderNumber % MODULO_7 == 0
+                ? null
+                : String.valueOf(orderNumber);
     }
 }
